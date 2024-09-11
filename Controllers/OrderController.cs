645a323ce88c4c11a2152b0d97c7cfefa08b138b
Gogovski20 <vladimir_gogovski@hotmail.com>
2024-09-11 21:09:34 +0200

    ExportAllOrders added

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using BookStoreAdminApplication.Models;
+using ClosedXML.Excel;
 using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing.Template;
@@ -73,6 +74,48 @@ namespace BookStoreAdminApplication.Controllers
             var stream = new MemoryStream();
             document.Save(stream, new PdfSaveOptions());
             return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportedInvoice.pdf");
+        }
+
+        [HttpGet]
+        public FileContentResult ExportOrders()
+        {
+            string fileName = "Orders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using(var workbook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workbook.Worksheets.Add("All Orders");
+                worksheet.Cell(1,1).Value = "Order Id";
+                worksheet.Cell(1, 2).Value = "Customer Email";
+
+                HttpClient client = new HttpClient();
+                string URL = "http://localhost:5173/api/Admin/GetAllOrders";
+                HttpResponseMessage response = client.GetAsync(URL).Result;
+
+                var data = response.Content.ReadAsAsync<List<Order>>().Result;
+
+                for(int i = 1; i <= data.Count(); i++)
+                {
+                    var item = data[i-1];
+
+                    worksheet.Cell(i+1, 1).Value = item.Id.ToString();
+                    worksheet.Cell(i+1, 2).Value = item.Owner.Email;
+
+                    for(int b = 0; b < item.BooksInOrder.Count(); b++)
+                    {
+                        worksheet.Cell(1, b + 3).Value = "Book-" + (b + 1);
+                        worksheet.Cell(i + 1, b + 3).Value = item.BooksInOrder.ElementAt(b).Book.Title;
+                    }
+                }
+                using(var stream = new MemoryStream()) 
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return File(content, contentType, fileName);
+                }
+            }
+            
         }
 
     }
